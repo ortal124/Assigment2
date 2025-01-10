2fececcfb01d44f38de8ea2854e7aec3227d8acb
ortal124 <ortal120400@gmail.com>
2025-01-10 11:14:42 +0200

    move salt to user service from controller

--- src/controller/userConroller.ts
+++ src/controller/userConroller.ts
@@ -1,18 +1,9 @@
-import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 import userService from '../services/userService';
 
 const register = async (req: Request, res: Response) => {
     try {
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const userToCreate = {
-            username: req.body.username,            
-            email: req.body.email,
-            password: hashedPassword,
-        }
-        const user = await userService.createUser(userToCreate);
+        const user = await userService.createUser(req.body);
         res.status(200).send(user);
     } catch (err) {
         res.status(400).send(err);
